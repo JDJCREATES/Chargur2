1375c89324888e3f0b2ef1214fb7ade1466e9e57
Jacob Jones <jdj.creates@gmail.com>
2025-06-24 06:52:42 -0500

    Updated DraggableConnectableWrapper.tsx

--- src/components/canvas/DraggableConnectableWrapper.tsx
+++ src/components/canvas/DraggableConnectableWrapper.tsx
@@ -147,7 +147,7 @@ export const DraggableConnectableWrapper: React.FC<DraggableConnectableWrapperPr
             <motion.div
               className="absolute inset-0 rounded-lg pointer-events-none ring-8 ring-teal-400/20"
               animate={{ scale: [1, 1.04, 1], opacity: [0, 1, 0] }}
-              transition={{ duration: 2.5, repeat: Infinity, ease: "easeInOut", delay: 0 }}
+              transition={{ duration: 2.1, repeat: Infinity, ease: "easeInOut", delay: 0 }}
             />
 
                <motion.div
@@ -161,7 +161,7 @@ export const DraggableConnectableWrapper: React.FC<DraggableConnectableWrapperPr
             <motion.div
               className="absolute inset-0 rounded-lg pointer-events-none ring-2 ring-yellow-600/60"
               animate={{ scale: [1, 1.05, 1], opacity: [0, .8, 0] }}
-              transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut", delay: 0.3 }}
+              transition={{ duration: 1.7, repeat: Infinity, ease: "easeInOut", delay: 0.3 }}
             />
           </>
         )}
