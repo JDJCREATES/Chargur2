96be012457bb9013dfa2a86281b314ae1ecdbf25
Jacob Jones <jdj.creates@gmail.com>
2025-06-24 06:49:39 -0500

    Updated DraggableConnectableWrapper.tsx

--- src/components/canvas/DraggableConnectableWrapper.tsx
+++ src/components/canvas/DraggableConnectableWrapper.tsx
@@ -146,9 +146,16 @@ export const DraggableConnectableWrapper: React.FC<DraggableConnectableWrapperPr
             {/* Outer Ring */}
             <motion.div
               className="absolute inset-0 rounded-lg pointer-events-none ring-8 ring-blue-400/20"
-              animate={{ scale: [1, 1.15, 1], opacity: [0, 1, 0] }}
+              animate={{ scale: [1, 1.04, 1], opacity: [0, 1, 0] }}
               transition={{ duration: 2.5, repeat: Infinity, ease: "easeInOut", delay: 0 }}
             />
+
+               <motion.div
+              className="absolute inset-0 rounded-lg pointer-events-none ring-8 ring-teal-400/20"
+              animate={{ scale: [1, 1.04, 1], opacity: [0, 1, 0] }}
+              transition={{ duration: 2.5, repeat: Infinity, ease: "easeInOut", delay: .8 }}
+            />
+            
             
             {/* Middle Ring */}
             <motion.div
@@ -159,8 +166,8 @@ export const DraggableConnectableWrapper: React.FC<DraggableConnectableWrapperPr
             
             {/* Inner Ring */}
             <motion.div
-              className="absolute inset-0 rounded-lg pointer-events-none ring-2 ring-blue-600/60"
-              animate={{ scale: [1, 1.04, 1], opacity: [0, 1, 0] }}
+              className="absolute inset-0 rounded-lg pointer-events-none ring-2 ring-yellow-600/60"
+              animate={{ scale: [1, 1.04, 1], opacity: [0, .8, 0] }}
               transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut", delay: 0.6 }}
             />
           </>
