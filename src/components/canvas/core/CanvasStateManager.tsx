d6da610fe48a3f321b01941b3ec8b5dd599e7344
Jacob Jones <jdj.creates@gmail.com>
2025-06-24 20:59:24 -0500

    Updated CanvasStateManager.tsx

--- src/components/canvas/core/CanvasStateManager.tsx
+++ src/components/canvas/core/CanvasStateManager.tsx
@@ -188,6 +188,7 @@ export const useCanvasStateManager = (
     
     const processorState: ProcessorState = {
       nodes: nodes
+    }
 
     CanvasDataProcessor.updateCanvasFromStageData(
       stageData,
